1b440dda8281a9a45068a9561fdad475ac7d336c
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-08-12 22:27:08 +0530

    Add files via upload
    3rd day 1st

---
+++ 3.1.c
@@ -0,0 +1,29 @@
+
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Temperature in Fahrenheit = %.2f", fahrenheit);
+
+    return 0;
+}
