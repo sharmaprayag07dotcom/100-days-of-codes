ebecca555b72c16a59f5bb27d5ca0811a8625120
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-08-16 15:38:23 +0530

    Add files via upload

---
+++ 7.1.c
@@ -0,0 +1,32 @@
+/*
+Sample Test Cases:
+Input 1:
+2020
+Output 1:
+Leap year
+
+Input 2:
+1900
+Output 2:
+Not a leap year
+
+Input 3:
+2000
+Output 3:
+Leap year
+
+*/#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+        printf("%d is a leap year.", year);
+    else
+        printf("%d is not a leap year.", year);
+
+    return 0;
+}
