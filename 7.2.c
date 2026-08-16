75e32ae707c29a45a88c9de6615351d6675e0db3
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-08-16 15:38:43 +0530

    Add files via upload

---
+++ 7.2.c
@@ -0,0 +1,29 @@
+/*
+Sample Test Cases:
+Input 1:
+a
+Output 1:
+Vowel
+
+Input 2:
+b
+Output 2:
+Consonant
+
+*/
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        printf("%c is a vowel.", ch);
+    else
+        printf("%c is a consonant.", ch);
+
+    return 0;
+    }
