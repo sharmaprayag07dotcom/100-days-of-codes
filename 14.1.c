d6a0dc9a30768f7e0fc1e5571ed3acd31b55b2ee
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-08-23 22:46:31 +0530

    Add files via upload

---
+++ 14.1.c
@@ -0,0 +1,29 @@
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+9
+
+Input 2:
+5
+Output 2:
+25
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum += 2 * i - 1;
+    }
+
+    printf("Sum of first %d odd numbers = %d\n", n, sum);
+
+    return 0;
+}
