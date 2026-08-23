31cd1afa74a1999972f2d9c3b336aa2fce3f762f
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-08-23 22:46:10 +0530

    Add files via upload

---
+++ 13.2.c
@@ -0,0 +1,27 @@
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+1 2 3 4 5
+
+Input 2:
+3
+Output 2:
+1 2 3
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
