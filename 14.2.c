18b13a968bf8b843ec5db772b47881aa69b1f64e
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-08-23 22:46:49 +0530

    Add files via upload

---
+++ 14.2.c
@@ -0,0 +1,30 @@
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+8 (2 * 4)
+
+Input 2:
+6
+Output 2:
+48 (2 * 4 * 6)
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long product = 1;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+
+    printf("Product of even numbers from 1 to %d = %lld\n", n, product);
+
+    return 0;
+}
