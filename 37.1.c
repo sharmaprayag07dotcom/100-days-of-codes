a5c5b9f616aea14e128a0771b51131044c416c2d
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-09-15 11:30:20 +0530

    Add files via upload

---
+++ 37.1.c
@@ -0,0 +1,34 @@
+/* Find the sum of each row of a matrix and store it in an array.
+
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+6 15
+
+*/
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols], sum[rows];
+
+    for (int i = 0; i < rows; i++) {
+        sum[i] = 0;
+
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum[i] += matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        printf("%d ", sum[i]);
+    }
+
+    return 0;
+}
---
+++ 37.2.c
@@ -0,0 +1,41 @@
+/* Find the transpose of a matrix.
+
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+1 4
+2 5
+3 6
+
+*/
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print transpose: columns become rows
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            printf("%d", matrix[i][j]);
+
+            if (i < rows - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
