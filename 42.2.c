06e703610c67b1ff10c2f7536ea10f1d2c730837
sharmaprayag07dotcom <www.sharmaprayag07@gmail.com>
2026-09-20 23:21:56 +0530

    Add files via upload

---
+++ 42.2.c
@@ -0,0 +1,32 @@
+/* Convert a lowercase string to uppercase without using built-in functions.
+
+
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+HELLO
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("%s", str);
+
+    return 0;
+}s
